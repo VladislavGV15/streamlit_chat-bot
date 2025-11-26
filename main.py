0a8e483cc6287392d952fec8e57c3959cc738a90
VladislavGV15 <155772255+VladislavGV15@users.noreply.github.com>
2025-11-26 22:10:38 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="Босс качалки")
+st.set_page_config(page_title="Инфо-без Чат-бот")
 st.title("Чат-бот по информационной безопасности")
+st.write(st.text_input("Введите свой запрос :"))
 
